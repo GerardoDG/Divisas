cc852d911a79612ba9d11670382c2a3ddd51b318
GerardoDG <102759351+GerardoDG@users.noreply.github.com>
2022-08-16 13:23:37 -0500

    primera modificacion

--- ejemplo/Program.cs
+++ ejemplo/Program.cs
@@ -12,7 +12,7 @@ class Program
 
         var client = new RestClient("https://api.apilayer.com/exchangerates_data/latest?symbols=EUR,MXN,BTC&base=USD"); //URL de la API
         RestRequest request = new RestRequest();
-        request.AddHeader("apikey", "X3MYhqjh8WHwplF0ZSPcDvySyDafNOkO");//Modificar segundo parámetro por su llave
+        request.AddHeader("apikey", "eA2FoPNIG4Vg0FYj1RUEIRU540Ugvgtn");//Modificar segundo parámetro por su llave
 
         var response = client.Execute(request);
         Root divisas = JsonConvert.DeserializeObject<Root>(response.Content);
@@ -20,8 +20,8 @@ class Program
         Console.WriteLine(divisas.rates.MXN);//Imprimir precio del dolar en pesos
         Console.WriteLine(divisas.rates.EUR);//Imprimir precio del dolar en euros
         Console.WriteLine(divisas.rates.BTC);//Imprimir precio del dolar en BTC
+        Console.WriteLine(divisas.rates.MXN);
 
-
-
+        
     }
 }
